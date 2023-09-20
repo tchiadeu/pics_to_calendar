96e84726c1151ffecb7bdd5e4221eebeec4b2cb7
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-20 18:22:10 +0200

    maybe ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
+
 class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
+    client = Google::Cloud::Vision::V1::ImageAnnotator::Client.new
+    image = { content: File.read('app/assets/images/test.png') }
+    @response = client.text_detection(image: image)
   end
 end
